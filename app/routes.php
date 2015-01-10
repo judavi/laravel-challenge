b1feb66b794b7e4f7c2be1a0fc2da55a9e3133e6
Diego Navarro <diego.nava07@gmail.com>
2015-01-10 01:25:10 -0500

    Initial route configuration

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,16 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', ['as' => 'index', 'uses' => 'MainPageController@index']);
+
+Route::post('auth', ['as' => 'auth', 'uses' => 'AuthController@auth']);
+
+Route::get('entries/{slug}',['as' => 'entries.show', 'uses' => 'EntriesController@show']);
+Route::resource('entries', 'EntriesController', ['except' => 'show', 'index']);
+
+Route::get('users/{slug}',['as' => 'users.show', 'uses' => 'UsersController@show']);
+Route::get('users/register',['as' => 'users.create', 'uses' => 'UsersController@create']);
+Route::get('users/password-remainder',['as' => 'users.password.remainder', 'uses' => 'UsersController@passwordRemainder']);
+Route::post('password-remainder',['as' => 'send.new.password', 'uses' => 'UsersController@sendNewPassword']);
+Route::resource('users', 'UsersController', ['except' => 'show', 'index', 'create']);
+
