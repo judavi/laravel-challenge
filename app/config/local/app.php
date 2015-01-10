d0ca17684b4c53dd8c64f97d0e78cf95901d430f
Diego Navarro <diego.nava07@gmail.com>
2015-01-10 01:23:29 -0500

    Add some tools to local dev

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -27,6 +27,22 @@ return array(
     */
 
     'providers' => array(
-        'Way\Generators\GeneratorsServiceProvider'
+        'Way\Generators\GeneratorsServiceProvider',
+        'Barryvdh\Debugbar\ServiceProvider',
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
+    
+    'aliases' => array(
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
     )
 );
